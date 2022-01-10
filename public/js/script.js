a51123d2f126e22062e4f553e90c4867338a0939
Kythadrin <vadimsiric@gmail.com>
2022-01-10 21:54:23 +0200

    Cleaning

--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,7 @@ function showAddresses(client) {
     const xhttp = new XMLHttpRequest();
 
     xhttp.onload = function() {
-      document.getElementById("clientAddress").innerHTML = this.responseText;
+        document.getElementById("clientAddress").innerHTML = this.responseText;
     }
     
     xhttp.open("GET", "/addresses?client=" + client);
