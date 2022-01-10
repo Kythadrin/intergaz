24d83efc090879e351aa115ed5adbc6ca109a2a3
Kythadrin <vadimsiric@gmail.com>
2022-01-10 21:16:16 +0200

    Router refactoring

--- conf/Config.php
+++ conf/Config.php
@@ -6,3 +6,31 @@ define("DB_HOST", 'local');
 define("DB_USER", '');
 define("DB_PASSWORD", '');
 define("DB_NAME", 'intergaz');
+
+function getControllerName() 
+{
+    $controllerName = "IndexController";
+
+    $route = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+    for ($i = 0; $i < count($route); $i++) {
+        if ($route[$i] != "") {
+            $name = '';
+
+            if (strripos($route[1], '_')) {
+                $path = explode("_", $route[1]);
+
+                for($i = 0; $i < count($path); $i++) {
+                    $name .= ucfirst($path[$i]);
+                }
+
+            } else {
+                $name = ucfirst($route[1]);
+            }
+
+            $controllerName = $name . "Controller";
+        }
+    }
+
+    return $controllerName;
+}
--- public/index.php
+++ public/index.php
@@ -1,39 +1,10 @@
 <?php
-session_start();
-
-include '../conf/Config.php';
-
+require_once '../conf/Config.php';
 require_once ROOT . '/vendor/autoload.php';
 
-$controllerName = "IndexController";
-$action = "loadPage";
+session_start();
 
-$route = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$controllerName = "Intergaz\Controllers\\" . getControllerName();
 
-for ($i = 0; $i < count($route); $i++) {
-    if ($route[$i] != "") {
-        $name = '';
-
-        if (strripos($route[1], '_')) {
-            $path = explode("_", $route[1]);
-
-            for($i = 0; $i < count($path); $i++) {
-                $name .= ucfirst($path[$i]);
-            }
-
-        } else {
-            $name = ucfirst($route[1]);
-        }
-
-        $controllerName = $name . "Controller";
-    }
-}
-
-if (isset($route[2]) && $route[2] != '') {
-    $action = $route[2];
-}
-
-$object = "Intergaz\Controllers\\" . $controllerName;
-
-$controller = new $object;
-$controller->$action();
+$controller = new $controllerName;
+$controller->loadPage();
