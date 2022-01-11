df789485d6fcafa7231470eca08cb96fffdedb67
Kythadrin <vadimsiric@gmail.com>
2022-01-11 21:26:04 +0200

    Refactoring

--- src/Views/addresses.tpl.php
+++ src/Views/addresses.tpl.php
@@ -1,8 +1,6 @@
 <div class="title">
     <h2><?php echo $pageData['client-name']?> adreses :</h2>
 </div>
-<ol id="addresses" class="list-group list-group-numbered">
 <?php
     $controller->displayDataAsList($pageData['addresses']);
 ?>
-</ol>
